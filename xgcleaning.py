bde8a0df0fc800084775badd880089ea8d695d02
Will Trevarthen <williamtrevarthen@hotmail.com>
2025-11-07 10:43:34 +0000

    cleaning

--- xgcleaning.py
+++ xgcleaning.py
@@ -1,59 +1,61 @@
 import pandas as pd
-df = pd.read_csv('xgpermatch.csv')
 
-df = df[df['Home xG'] != 'xG']
-df = df.reset_index(drop=True)
+if __name__ == '__main__':
+    df = pd.read_csv('sup data/xgpermatch.csv')
 
-print(df.head())
+    df = df[df['Home xG'] != 'xG']
+    df = df.reset_index(drop=True)
 
-team_mapping = {
-    'Arsenal FC': 'Arsenal',
-    'Aston Villa': 'Aston Villa',
-    'AFC Bournemouth': 'Bournemouth',
-    'Brentford FC': 'Brentford',
-    'Brighton & Hove Albion': 'Brighton',
-    'Burnley FC': 'Burnley',
-    'Cardiff City': 'Cardiff',
-    'Chelsea FC': 'Chelsea',
-    'Crystal Palace': 'Crystal Palace',
-    'Everton FC': 'Everton',
-    'Fulham FC': 'Fulham',
-    'Huddersfield Town': 'Huddersfield',
-    'Hull City': 'Hull',
-    'Ipswich Town': 'Ipswich',
-    'Leeds United': 'Leeds',
-    'Leeds Utd': 'Leeds',
-    'Leicester City': 'Leicester',
-    'Liverpool FC': 'Liverpool',
-    'Manchester City': 'Man City',
-    'Manchester United': 'Man United',
-    'Manchester Utd': 'Man United',
-    'Middlesbrough FC': 'Middlesbrough',
-    'Newcastle United': 'Newcastle',
-    'Newcastle Utd': 'Newcastle',
-    'Norwich City': 'Norwich',
-    'Southampton FC': 'Southampton',
-    'Swansea City': 'Swansea',
-    'Stoke City': 'Stoke',
-    'Sunderland AFC': 'Sunderland',
-    'Sheffield Utd': 'Sheffield United',
-    'Tottenham Hotspur': 'Tottenham',
-    'Watford FC': 'Watford',
-    'West Bromwich Albion': 'West Brom',
-    'West Ham United': 'West Ham',
-    'West Ham Utd': 'West Ham',
-    'Wolverhampton Wanderers': 'Wolves',
-    'Nottingham Forest': "Nott'm Forest",
-    "Nott'ham Forest": "Nott'm Forest",
-    'Luton Town': 'Luton',
-    'Ipswich Town': 'Ipswich',
-    'Fulham FC': 'Fulham',
-    'Spurs': 'Tottenham',
-    'Man Utd': 'Man United'
-}
+    print(df.head())
 
-df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+    team_mapping = {
+        'Arsenal FC': 'Arsenal',
+        'Aston Villa': 'Aston Villa',
+        'AFC Bournemouth': 'Bournemouth',
+        'Brentford FC': 'Brentford',
+        'Brighton & Hove Albion': 'Brighton',
+        'Burnley FC': 'Burnley',
+        'Cardiff City': 'Cardiff',
+        'Chelsea FC': 'Chelsea',
+        'Crystal Palace': 'Crystal Palace',
+        'Everton FC': 'Everton',
+        'Fulham FC': 'Fulham',
+        'Huddersfield Town': 'Huddersfield',
+        'Hull City': 'Hull',
+        'Ipswich Town': 'Ipswich',
+        'Leeds United': 'Leeds',
+        'Leeds Utd': 'Leeds',
+        'Leicester City': 'Leicester',
+        'Liverpool FC': 'Liverpool',
+        'Manchester City': 'Man City',
+        'Manchester United': 'Man United',
+        'Manchester Utd': 'Man United',
+        'Middlesbrough FC': 'Middlesbrough',
+        'Newcastle United': 'Newcastle',
+        'Newcastle Utd': 'Newcastle',
+        'Norwich City': 'Norwich',
+        'Southampton FC': 'Southampton',
+        'Swansea City': 'Swansea',
+        'Stoke City': 'Stoke',
+        'Sunderland AFC': 'Sunderland',
+        'Sheffield Utd': 'Sheffield United',
+        'Tottenham Hotspur': 'Tottenham',
+        'Watford FC': 'Watford',
+        'West Bromwich Albion': 'West Brom',
+        'West Ham United': 'West Ham',
+        'West Ham Utd': 'West Ham',
+        'Wolverhampton Wanderers': 'Wolves',
+        'Nottingham Forest': "Nott'm Forest",
+        "Nott'ham Forest": "Nott'm Forest",
+        'Luton Town': 'Luton',
+        'Ipswich Town': 'Ipswich',
+        'Fulham FC': 'Fulham',
+        'Spurs': 'Tottenham',
+        'Man Utd': 'Man United'
+    }
 
-df['Home'] = df['Home'].map(team_mapping).fillna(df['Home'])
-df['Away'] = df['Away'].map(team_mapping).fillna(df['Away'])
-df.to_csv('clean_xg.csv', index=False)
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+
+    df['Home'] = df['Home'].map(team_mapping).fillna(df['Home'])
+    df['Away'] = df['Away'].map(team_mapping).fillna(df['Away'])
+    df.to_csv('sup data/clean_xg.csv', index=False)
